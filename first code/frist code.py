70e72c98a6e958c47158aed213571d1451bfe9bb
Joojo Stephens <kojoarmahnathaniel@gmail.com>
2023-01-10 21:56:01 +0000

    update

--- first code/frist code.py
+++ first code/frist code.py
@@ -6,4 +6,8 @@ print("The capybara lives in groups")
 print("The capybara can swim")
 
 # Printing multiple lines of code with "\n"
+# "\n" brings the code to the next line on output
 print("Hickory Dickory Dock! \nThe mouse went up the clock")
+
+# "\t" inserts an indentation to the code on output
+print("Hickory Dickory Dock! \n\tThe mouse went up the clock")
