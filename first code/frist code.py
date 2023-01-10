bb72d6ae218895c655478b5458b77a79381efba7
Joojo Stephens <kojoarmahnathaniel@gmail.com>
2023-01-10 05:33:16 +0000

    update

--- first code/frist code.py
+++ first code/frist code.py
@@ -1,1 +1,9 @@
 print("Hello World")
+
+# Printing multiple lines of code with different lines
+print("The capybara is the world's largest rodent")
+print("The capybara lives in groups")
+print("The capybara can swim")
+
+# Printing multiple lines of code with "\n"
+print("Hickory Dickory Dock! \nThe mouse went up the clock")
