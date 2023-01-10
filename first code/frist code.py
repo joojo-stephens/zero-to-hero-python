d1247b5d25e45df97beb78d8dd6f073882502f27
Joojo Stephens <kojoarmahnathaniel@gmail.com>
2023-01-10 22:16:32 +0000

    update

--- first code/frist code.py
+++ first code/frist code.py
@@ -11,3 +11,9 @@ print("Hickory Dickory Dock! \nThe mouse went up the clock")
 
 # "\t" inserts an indentation to the code on output
 print("Hickory Dickory Dock! \n\tThe mouse went up the clock")
+
+# inserting a double quote in a string using a backslash(\)
+print("here is a double quote \"")
+
+# a case where we will need double backslashes(\\)
+print("here is the \\news")
