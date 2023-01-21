441e6f46aeb39579d51dbb8d8938169cb6ea4006
Joojo Stephens <kojoarmahnathaniel@gmail.com>
2023-01-21 23:24:38 +0000

    update

--- first code/Challenge 8 (While  loop)/Challenge 8.py
+++ first code/Challenge 8 (While  loop)/Challenge 8.py
@@ -1,9 +1,21 @@
 import turtle
 length = 0
 angle = 0
+pencolor = "red"
+length = input("Specify yuor length? (input 0 to stop drawing) ")
+length = int (length)
+angle = input("Speciify your angle: ")
+angle = int (angle)
+pencolor = input("What pen color do your want to use? ")
+     
 while length != 0:
-    length = input("Specify yuor length? ")
-    angle = input("Speciify your angle: ")
+    turtle.color(pencolor)
     turtle.forward(length)
     turtle.right(angle)
+    length = input("Specify yuor length? (input 0 to stop drawing) ")
+    length = int (length)
+    if length != 0:
+        angle = input("Speciify your angle: ")
+        angle = int (angle)
+        pencolor = input("What pen color do your want to use? ")
 print("nice")
